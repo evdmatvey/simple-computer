948a266be89e1a8ef6349cbbcaffc2b8a6314b85
Синицын Станислав Дмитриевич <ip312s23@csc.sibsutis.ru>
2025-02-24 23:20:31 +0700

    Add console.h

---
+++ console/console.h
@@ -0,0 +1,12 @@
+#ifndef CONSOLE_H
+
+#include "../include/mySimpleComputer.h"
+#include <stdio.h>
+
+void printCell (int address);
+void printDecodedCommand (int value);
+void printFlags (void);
+void printAccumulator (void);
+void printCounters (void);
+
+#endif 
